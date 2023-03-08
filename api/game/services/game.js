0b024c49ada36161880bb2ed9498e43d8bfb6dd3
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-03-08 17:47:38 -0300

    Buscando dados já criados e evitando erros de duplicação

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,6 +23,24 @@ async function getGameInfo(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+
+  return;
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -30,16 +48,7 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    await strapi.services.publisher.create({
-      name: products[2].publisher,
-      slug: slugify(products[2].publisher).toLowerCase(),
-    });
-
-    await strapi.services.developer.create({
-      name: products[2].developer,
-      slug: slugify(products[2].developer).toLowerCase(),
-    });
-
-    // console.log(products[2]);
+    await create(products[4].publisher, "publisher");
+    await create(products[4].developer, "developer");
   },
 };
